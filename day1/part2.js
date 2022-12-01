e399652d70b2a37be96f443f83cf5eb728863d28
ribdot <rick.fr.simon@gmail.com>
2022-12-01 15:36:07 -0700

    day 1 part 2

---
+++ day1/part2.js
@@ -0,0 +1,16 @@
+const { input } = require("./input");
+
+let totals = [];
+let sum = 0;
+input.forEach(calories => {
+    if (isNaN(calories)) {
+        totals.push(sum);
+        sum = 0;
+    } else {
+        sum += calories;
+    }
+});
+totals.push(sum);
+
+totals = totals.sort((a, b) => {return b - a });
+console.log(totals[0] + totals[1] + totals[2]);
