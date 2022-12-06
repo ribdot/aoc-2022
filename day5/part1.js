41bbb384a12d65e93530782854614801cf1edef7
ribdot <rick.fr.simon@gmail.com>
2022-12-05 17:41:57 -0700

    day 5 part 1

---
+++ day5/part1.js
@@ -0,0 +1,45 @@
+const { input } = require("./input.js");
+
+let stackInput = [];
+let moveInput = [];
+
+let hasFoundBlankLine = false;
+input.forEach(line => {
+    if (line == "") {
+        hasFoundBlankLine = true;
+    } else if (!hasFoundBlankLine) {
+        stackInput.push(line.match(/.{1,4}/g));
+    } else if (hasFoundBlankLine) {
+        moveInput.push(line.match(/\d+/g).map(num => parseInt(num)));
+    }
+});
+
+let stacks = [];
+stackInput.reverse().forEach((row, key) => {
+    if (key == 0) {
+        row.forEach(num => {
+            stacks.push([]);
+        });
+    } else {
+        for(let i=0; i< row.length; i++) {
+            let crate = row[i].trim();
+            if (crate !== '') {
+                stacks[i].push(crate);
+            }
+        }
+    }
+});
+
+moveInput.forEach(move => {
+    for(let i = 0; i < move[0]; i++) {
+        let crate = stacks[move[1] - 1].pop();
+        stacks[move[2] - 1].push(crate);
+    }
+});
+
+let tops = "";
+stacks.forEach(stack => {
+    tops += stack.pop().charAt(1);
+});
+
+console.log(tops);
