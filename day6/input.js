d4892d3ca7d0114ffcc68ae865e8d97f68220d50
ribdot <rick.fr.simon@gmail.com>
2022-12-06 17:41:53 -0700

    day 6 part 1

---
+++ day6/input.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const fs = require("fs");
+
+const args = process.argv.slice(2);
+
+var inputFile = "input.txt";
+if (args.length) {
+    inputFile = args[0];
+}
+
+const input = fs.readFileSync(path.resolve(__dirname, inputFile), "utf-8", (err, data) => {
+    if (err) {
+        console.error(err);
+        return "0";
+    }
+
+    return data;
+}).toString();
+
+module.exports ={
+    input
+};
